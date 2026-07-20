e9382c754d7a709f41754d5fc4f12113f46aa226
Shashank Lakhera <lakherashashank70@gmail.com>
2026-07-21 02:25:54 +0530

    Operators in JS

---
+++ JavaScript/operators.js
@@ -0,0 +1,66 @@
+// Operators
+/*
+| Operator | Meaning             | Example  |
+| -------- | ------------------- | -------- |
+| `+`      | Addition            | `10 + 5` |
+| `-`      | Subtraction         | `10 - 5` |
+| `*`      | Multiplication      | `10 * 5` |
+| `/`      | Division            | `10 / 5` |
+| `%`      | Modulus (remainder) | `10 % 3` |
+| `**`     | Exponent (power)    | `2 ** 3` |
+ 
+*/
+
+// let age = 45;
+
+// console.log("He join CJP = ", age >= 18 && age <= 40); 
+
+// AND operator
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
+
+// OR Operator
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+// let color = "red"
+
+// console.log(color === "yellow" || color === "green");
+
+
+// NOT Operator
+// !true = false
+// !false = true
+
+
+/* Increment & Decrement */
+
+let x = 5;
+
+// console.log(x++);
+// console.log(x);
+
+
+console.log(x--);
+console.log(x);
+
+
+
+x = x + 1;
+x++;
+x += 1;
+
+x = x - 1;
+x--;
+x -= 1;
+
+x = x * 2;
+x *= 2;
+
+x = x / 2;
+x /= 2;
