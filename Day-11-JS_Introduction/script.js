911fd0f566881f056e8541e82a4200eaa1c358c6
Shashank Lakhera <lakherashashank70@gmail.com>
2026-07-20 23:57:35 +0530

    Update in js

--- Day-11-JS_Introduction/script.js
+++ Day-11-JS_Introduction/script.js
@@ -1,6 +1,8 @@
+// Data Types
+
 // There are two types of data type
 // 1 Primitive DataType => String, Number, Boolean, Undefined, Null, BigInt, Symbol
-// 2 Non-Primitive DataTtpe => are created usign primitive datatype - Object 
+// 2 Non-Primitive DataTtpe => are created usign primitive datatype - Object
 
 //  Object = {
 // key:value,
@@ -8,19 +10,26 @@
 // key:value,
 // }
 
-
 let student = {
-    name : "Shashank",
-    age : 21,
-    City : "Bhopal"
-}
+  name: "Shashank",
+  age: 21,
+  City: "Bhopal",
+};
 
 let student2 = {
-    name:"Shobhit",
-    city:"Bhopal",
-    age:21
-}
+  name: "Shobhit",
+  city: "Bhopal",
+  age: 21,
+};
 
 // console.log(student2.name);
+console.log(typeof student2);
 
-console.log(student2.name+" is of age "+ student2.age + " and is living in "+student2.city+".");
+console.log(
+  student2.name +
+    " is of age " +
+    student2.age +
+    " and is living in " +
+    student2.city +
+    ".",
+);
