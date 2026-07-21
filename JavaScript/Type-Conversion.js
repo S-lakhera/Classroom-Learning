61b63ffaea9cadcc9f209c84263ff314903e7d9b
Shashank Lakhera <lakherashashank70@gmail.com>
2026-07-22 02:24:49 +0530

    Type-conversion in JS

---
+++ JavaScript/Type-Conversion.js
@@ -0,0 +1,68 @@
+// Type COnversion
+
+// let a = 15;
+
+// let age = Number(a);
+
+// a = "Aman";
+
+// console.log(age)
+
+// Implicit Type Conversion (Type coercion)
+
+// console.log("5" + 2); // "5" + "2" = "52"; Concatenation
+// console.log("5" - 2); // "5" - "2" = 3; Subtraction)
+// console.log("5" * 2); // "5" * "2" = 10; Multiplication
+// console.log("5" / 2); // "5" / "2" = 2.5
+
+
+// Boolean Conversion
+// false = 0
+// true = 1
+
+// console.log(true + 1); // 1 + 1 = 2 
+// console.log(false - 1); // 0 - 1 = -1
+
+
+// Explicit Type COnversion
+
+// let age = "22";
+
+// let num = Number(age);
+
+// console.log(num);
+
+// console.log("Type of age is = ", typeof age);
+
+// console.log("Type of num is = ", typeof num);
+
+
+// Invalid COnversions
+// let a = "Hello";
+
+// let num = Number(a);
+
+// console.log(num);
+// console.log(typeof num);
+
+
+// Number to String Conversion
+// let a = 100;
+
+// let num = String(a);
+
+// console.log(a);
+// console.log(num);
+// console.log(typeof num);
+
+
+// console.log(Boolean(undefined))
+
+
+// console.log(parseInt("10.04px4454"));
+// console.log(parseFloat("10.04px4454"));
+
+
+console.log(Number(""));
+
+console.log(Boolean(" "));
